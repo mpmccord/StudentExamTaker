a77ee1813e626d1c972b57678bac460d87431784
Melanie McCord <melanie.mccord19@ncf.edu>
2021-11-19 19:29:16 -0500

    Added templates for pages

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
+
 @app.route("/")
 def main_page():
     return render_template("index.html")
 
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    return render_template("signup.html")
