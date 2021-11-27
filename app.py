7b8efb4f4c7d3330b296f871237ea8446c874aaa
Melanie McCord <melanie.mccord19@ncf.edu>
2021-11-27 18:53:46 -0500

    Basic login now working

--- app.py
+++ app.py
@@ -17,10 +17,31 @@ def main_page():
     return render_template("index.html")
 
 
-@app.route("/login", methods=['GET', 'POST'])
+"""@app.route("/login", methods=['GET', 'POST'])
 def login():
     if flask.request.method == 'GET':
         return render_template("login.html")
+    email = flask.request.form['email']
+    if flask.request.form['password'] == users[email]['password']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('protected'))
+
+    return 'Bad login'"""
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if flask.request.method == 'GET':
+        return '''
+               <form action='login' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               '''
+
     email = flask.request.form['email']
     if flask.request.form['password'] == users[email]['password']:
         user = User()
