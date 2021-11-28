e5f6d45863f84de4e6e82244117773be5074a819
Melanie McCord <melanie.mccord19@ncf.edu>
2021-11-27 19:06:40 -0500

    Added HTML templates for bad logins

--- app.py
+++ app.py
@@ -22,13 +22,15 @@ def login():
     if flask.request.method == 'GET':
         return render_template("login.html")
     email = flask.request.form['email']
+    if email not in users:
+        return render_template("bad-login.html")
     if flask.request.form['password'] == users[email]['password']:
         user = User()
         user.id = email
         flask_login.login_user(user)
         return flask.redirect(flask.url_for('protected'))
 
-    return 'Bad login'
+    return render_template("bad-login.html")
 
 
 """
